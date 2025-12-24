388f5449e9e6e364be7a7dc66bea97c0e2e12306
CrezyDeveloperz <117562112+CrezyDeveloperz@users.noreply.github.com>
2025-12-24 17:12:08 +0530

    Update vars.py

--- Dxbots/vars.py
+++ Dxbots/vars.py
@@ -12,7 +12,7 @@ class Var(object):
     name = str(getenv('name', 'Tesa9101_bot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002723743584'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002033900774'))
     PORT = int(getenv('PORT', 8080))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
@@ -35,7 +35,8 @@ class Var(object):
         #URL = "https://{}/".format(FQDN)
         URL = FQDN 
     DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://abhishekkumar040440:abhishek0440@cluster0.iuuk7rt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'))
-    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', '')) 
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'only_botz')) 
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "")).split())) 
+
 
 
